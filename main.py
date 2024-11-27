19b8367b066b72e2dc302c6a9d4184d580c99325
Karu48 <rodrigo.rln.04@gmail.com>
2024-11-26 19:08:55 -0500

    feat(backend): auth

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 from bcrypt import hashpw, gensalt
+import uuid
 
 app = Flask(__name__)
 
@@ -12,32 +13,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(200), nullable=False)
+    id = db.Column(db.String(200), primary_key=True, default = str(uuid.uuid4()))
+    username = db.Column(db.String(200), nullable=False)
     passwordHash = db.Column(db.String(200), nullable=False)
+    cardId = db.Column(db.String(200), nullable=True)
+    tickets = db.Column(db.Integer, default=0)
+    credits = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     
     def __repr__(self):
-        return '<User %r>' % self.id
+        return f"{self.username}: {self.id}, {self.passwordHash}, {self.cardId}, {self.tickets}, {self.credits}, {self.date_created}"
     
 class Session(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    id = db.Column(db.String(200), primary_key=True, default = str(uuid.uuid4()))
+    userId = db.Column(db.String(200), db.ForeignKey('user.id'), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     
     def __repr__(self):
-        return '<Session %r>' % self.id
+        return f"Session for user {self.userId}: {self.id}, {self.date_created}"
+    
+@app.route('/debug')
+def printAll():
+    users = User.query.all()
+    sessions = Session.query.all()
+    print("Users:")
+    for user in users:
+        print(user)
+    print("Sessions:")
+    for session in sessions:
+        print(session)
+    return "Check console for output"
     
 @app.route('/auth/register', methods=['POST'])
 def register():
     if request.method == 'POST':
         data = request.json
-        name = data['name']
+        username = data['username']
         email = data['email']
         password = data['password']
-        user = User(name=name, email=email, passwordHash=hashpw(password.encode('utf-8'), gensalt()).decode('utf-8'))
+        user = User(username=username, email=email, passwordHash=hashpw(password.encode('utf-8'), gensalt()).decode('utf-8'))
         db.session.add(user)
         db.session.commit()
 
@@ -47,9 +63,13 @@ def register():
 def login():
     if request.method == 'POST':
         data = request.json
-        email = data['email']
+        username = data['username']
         password = data['password']
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
+        userCheck = Session.query.filter_by(userId=user.id).first()
+        if userCheck:
+            db.session.delete(userCheck)
+            db.session.commit()
         if user and hashpw(password.encode('utf-8'), user.passwordHash.encode('utf-8')) == user.passwordHash.encode('utf-8'):
             session = Session(userId=user.id)
             db.session.add(session)
@@ -73,6 +93,19 @@ def check_session(sessionId):
         return True
     else:
         return False
+
+@app.route('/user', methods=['GET'])
+def getUserData():
+    args = request.args
+    sessionInside = Session(id=args.get("sessionId"))
+    user = User(id=sessionInside.userId)
+    exists = db.session.query(db.exists().where(User.id == sessionInside.userId)).scalar()
+    if (exists == False):
+        return jsonify({'error': 'session not found'}) 
+    print(user)
+    return jsonify({'userId': user.id, 'tickets': user.tickets, 'credits': user.credits})
     
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run()
